59c363fee5ca5359b487f526bab2491d011e12e9
Eduardo Sampaio <edupcsampaio@gmail.com>
2025-11-10 18:19:23 -0300

    create Badge component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Badge } from './components/badge'
 import { Text } from './components/text'
 
 export function App() {
@@ -6,6 +7,8 @@ export function App() {
       <Text as="p" variant="body-md-bold" className="text-pink-base">
         Hello World
       </Text>
+      <Badge variant="secondary">0</Badge>
+      <Badge>1 de 5</Badge>
     </div>
   )
 }
